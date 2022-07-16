be19620e20170bd806bb075975fba4e13ddd7edf
nullbuddy1243 <nullbuddy1243@no.com>
2022-07-16 16:54:57 -0400

    lil removals

--- sbmilady/src/components/Box/Box.js
+++ sbmilady/src/components/Box/Box.js
@@ -4,10 +4,7 @@ import SkillCard from "../SkillCard";
 export default function Box() {
   return (
     <div className="Box">
-      {/* <p className="boxflag">Box Skill </p> */}
-      {/* <h5 className="boxflag">Box Skill H5 </h5> */}
       <SkillCard />
-      {/* <img src={stronger} className="" alt="logo" /> */}
     </div>
   );
 }
