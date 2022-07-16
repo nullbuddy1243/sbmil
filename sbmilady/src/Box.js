3d7e7fd77de83fc6f1b40c8d2ca00164985b9a6b
nullbuddy1243 <nullbuddy1243@no.com>
2022-07-16 05:52:17 -0400

    playing with ui

---
+++ sbmilady/src/Box.js
@@ -0,0 +1,16 @@
+import stronger from "./stronger.jpeg";
+import "./App.css";
+import SkilLCard from "./SkillCard";
+
+function Box() {
+  return (
+    <div className="Box">
+      {/* <p className="boxflag">Box Skill </p> */}
+      <h5 className="boxflag">Box Skill H5 </h5>
+      <SkilLCard />
+      {/* <img src={stronger} className="" alt="logo" /> */}
+    </div>
+  );
+}
+
+export default Box;
