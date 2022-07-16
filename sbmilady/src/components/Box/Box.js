97fbbf7e44652de432ce32cc0746942ac2fb0de8
Ryan Shtirmer <rshtirmer@gmail.com>
2022-07-16 18:27:36 -0400

    map over skills - not ready for merge (#2)
    * map over skills
    
    * finish maps

--- sbmilady/src/components/Box/Box.js
+++ sbmilady/src/components/Box/Box.js
@@ -1,10 +1,30 @@
 // import "./App.css";
 import SkillCard from "../SkillCard";
+import athousandwords from "../../utils/25000words.js";
 
 export default function Box() {
+  const words = athousandwords.split(" ");
+  const mySkills = [
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)]
+  ]
+  const myNeeds = [
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)],
+    words[Math.floor(Math.random() * words.length)]
+  ]
+
   return (
     <div className="Box">
-      <SkillCard />
+      <SkillCard 
+        skills={mySkills}
+        needs={myNeeds}
+      />
     </div>
   );
 }
